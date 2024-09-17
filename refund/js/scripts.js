e74eadc951247cca34a275c88395247c3d2c1f55
Mariana Batista <mariana-batista99@hotmail.com>
2024-09-17 17:39:22 -0300

    criando a li

---
+++ refund/js/scripts.js
@@ -0,0 +1,48 @@
+const form = document.querySelector('form');
+const amount = document.getElementById('amount');
+const expense = document.getElementById('expense');
+const category = document.getElementById('category');
+
+amount.oninput = () => {
+  let value = amount.value.replace(/\D/g, '');
+
+  value = Number(value) / 100;
+
+  amount.value = formatCurrencyBRL(value);
+};
+
+function formatCurrencyBRL(value) {
+  value = value.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  });
+
+  return value;
+}
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date(),
+  };
+
+  expenseAdd(newExpense);
+};
+
+function expenseAdd(newExpense) {
+  try {
+    const expenseItem = document.createElement('li');
+    expenseItem.classList.add('expense');
+
+    
+  } catch (error) {
+    alert('Não foi possivel atualizar a lista de despesa');
+    console.log(error);
+  }
+}
